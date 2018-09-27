609ac2aec6924ef932dc4bbc9a57451eab88bb44
MGH <miriamhall.mgh@gmail.com>
2018-09-27 15:03:32 -0700

    added unit tests

--- page_spider.py
+++ page_spider.py
@@ -10,9 +10,10 @@ def main(database: str, url_list_file: str):
     urls = url_utilities.load_urls_from_file(url_list_file)
     for url in urls:
         print("Reading " + url)
-        page_content= url_utilities.load_page(url=url)
+        page_content = url_utilities.load_page(url=url)
         words = url_utilities.scrape_page(page_contents=page_content)
         big_word_list.extend(words)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
